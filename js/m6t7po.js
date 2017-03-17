265f05684dc3dd130a316525e0cb61dc0ad3647c
Terry Tataje <t5t6pe@gmail.com>
2017-03-17 15:38:48 -0500

    Tarea Cumplida
    subiendo la tarea

--- js/m6t7po.js
+++ js/m6t7po.js
@@ -13,6 +13,19 @@ $(function(){
     }
   });
 
+  $('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		},300 );
+	});
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 0){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		};
+	});
 
 $(function(a){
   if(typeof define==="function"&&define.amd&&define.amd.jQuery){
