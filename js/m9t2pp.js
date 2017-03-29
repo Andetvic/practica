6c34c7f46e420c6b36246dfc7323a5fe199bf610
Terry Tataje <t5t6pe@gmail.com>
2017-03-29 14:59:30 -0500

    funciona el codigo
    al 100% codigo nada de css por ahora

--- js/m9t2pp.js
+++ js/m9t2pp.js
@@ -1,24 +1,15 @@
 var map, lat, lng;
+var posicionGEO = [];
+
+localStorage.posiciones = (localStorage.posiciones || "");
 
 $(function(){
-/*
-  $(".oculto").hide();
-  $(".inf").click(function(){
-    var nodo = $(this).attr("href");
+  $('#info2').hide();
+  $('#info3').hide();
 
-    if ($(nodo).is(":visible")){
-      $(nodo).hide();
-      return false;
-    }else{
-      $(".oculto").hide("slow");
-      $(nodo).fadeToggle('fast');
-      return false;
-    }
-  });
-*/
   function enlazarMarcador(e){
-
-   // muestra ruta entre marcas anteriores y actuales
+    var alat, alng;
+    // muestra ruta entre marcas anteriores y actuales
     map.drawRoute({
       origin: [lat, lng],  // origen en coordenadas anteriores
       // destino en coordenadas del click o toque actual
@@ -29,10 +20,50 @@ $(function(){
       strokeWeight: 4
     });
 
+    alat = lat;
+    alng = lng;
     lat = e.latLng.lat();   // guarda coords para marca siguiente
     lng = e.latLng.lng();
 
+    posicionGEO.push([lat,lng]);
+    localStorage.posiciones = JSON.stringify(posicionGEO);
+
     map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
+  };
+
+  function reiniciar(){
+    map.cleanRoute();
+    map.removeMarkers();
+    map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
+    localStorage.posiciones = "";
+    posicionGEO = [];
+    geolocalizar();
+  };
+
+  function cargar(){
+    //Recuperamos las posiciones almacenadas
+    posicionGEO = JSON.parse(localStorage.posiciones);
+    //Pasamos el primer valor
+    lat = posicionGEO[0][0];
+    lng = posicionGEO[0][1];
+
+    map = new GMaps({ el: '#map', lat: lat, lng: lng, click: enlazarMarcador, tap: enlazarMarcador });
+    map.addMarker({lat: posicionGEO[0][0], lng: posicionGEO[0][1]});
+    //Recorremos todo el array para sacar el resto de puntos
+    for (var i = 1; i < posicionGEO.length; i++) {
+      map.drawRoute({
+        origin: [posicionGEO[i-1][0], posicionGEO[i-1][1]],
+        destination: [posicionGEO[i][0], posicionGEO[i][1]],
+        travelMode: 'driving',
+        strokeColor: '#ff0000',
+        strokeOpacity: 0.6,
+        strokeWeight: 4
+      });
+      //Marcamos el punto obtenido en el mapa y pasamos su valor a las variables lat y lng para que sean el origen para el siguiente
+      map.addMarker({lat: posicionGEO[i][0], lng: posicionGEO[i][1]});
+      lat = posicionGEO[i][0];
+      lng = posicionGEO[i][1];
+    };
   };
 
   function geolocalizar(){
@@ -40,7 +71,7 @@ $(function(){
       success: function(position){
         lat = position.coords.latitude;  // guarda coords en lat y lng
         lng = position.coords.longitude;
-
+        posicionGEO = [];
         map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
           el: '#map',
           lat: lat,
@@ -48,6 +79,8 @@ $(function(){
           click: enlazarMarcador,
           tap: enlazarMarcador
         });
+        posicionGEO.push([lat,lng]);
+        localStorage.posiciones = JSON.stringify(posicionGEO);
         map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
       },
       error: function(error) { alert('Ahí un error en la Geolocalización: '+error.message); },
@@ -55,5 +88,7 @@ $(function(){
     });
   };
 
-  geolocalizar();
+  $("#restablecer").on('click', reiniciar);
+
+  if (localStorage.posiciones.length != 0) { cargar();} else {geolocalizar();};
 });
--- js/m9t3po.js
+++ js/m9t3po.js
@@ -1,10 +1,12 @@
 var map, lat, lng;
+var posicionGEO = [];
+
+localStorage.posiciones = (localStorage.posiciones || "");
 
 $(function(){
-
   function enlazarMarcador(e){
-
-   // muestra ruta entre marcas anteriores y actuales
+    var alat, alng;
+    // muestra ruta entre marcas anteriores y actuales
     map.drawRoute({
       origin: [lat, lng],  // origen en coordenadas anteriores
       // destino en coordenadas del click o toque actual
@@ -15,10 +17,72 @@ $(function(){
       strokeWeight: 4
     });
 
+    alat = lat;
+    alng = lng;
     lat = e.latLng.lat();   // guarda coords para marca siguiente
     lng = e.latLng.lng();
 
+    posicionGEO.push([lat,lng]);
+    localStorage.posiciones = JSON.stringify(posicionGEO);
+
     map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
+  };
+
+  function reiniciar(){
+    map.cleanRoute();
+    map.removeMarkers();
+    map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
+    localStorage.posiciones = "";
+    posicionGEO = [];
+    geolocalizar();
+  };
+
+  function compactar(){
+    map.cleanRoute();
+    map.removeMarkers();
+    //Marcamos el punto de partida.
+    map.addMarker({lat:posicionGEO[0][0],lng:posicionGEO[0][1]});
+    //He cambiado el strokeColor, para que la la antigua ruta salga en rojo.
+    map.drawRoute({
+      origin:[posicionGEO[0][0],posicionGEO[0][1]],
+      destination: [lat, lng],
+      travelMode: 'driving',
+      strokeColor: '#ff0000',
+      strokeOpacity: 0.6,
+      strokeWeight: 4
+    });
+    posicionGEO.length=1;  //eliminamos todos los puntos salvo longtitud y latitud inicial
+    posicionGEO.push([lat,lng]);
+    localStorage.posiciones = "";
+    localStorage.posiciones = JSON.stringify(posicionGEO);
+    //Marcamos el último punto que teníamos guardado
+    map.addMarker({lat: lat, lng:lng});
+  };
+
+  function registra(){
+    //Recuperamos la ruta trazada anteriormente
+    posicionGEO = JSON.parse(localStorage.posiciones);
+    //Pasamos el valor del primer punto para centrar el mapa y marcar este punto en el mapa
+    lat = posicionGEO[0][0];
+    lng = posicionGEO[0][1];
+
+    map = new GMaps({ el: '#map', lat: lat, lng: lng, click: enlazarMarcador, tap: enlazarMarcador });
+    map.addMarker({lat: posicionGEO[0][0], lng: posicionGEO[0][1]});
+    //Recorremos todo el array para sacar el reto de puntos
+    for (var i = 1; i < posicionGEO.length; i++) {
+      map.drawRoute({
+        origin: [posicionGEO[i-1][0], posicionGEO[i-1][1]],
+        destination: [posicionGEO[i][0], posicionGEO[i][1]],
+        travelMode: 'driving',
+        strokeColor: '#ff0000',
+        strokeOpacity: 0.6,
+        strokeWeight: 4
+      });
+      //Marcamos el punto obtenido en el mapa y pasamos su valor a las variables lat y lng para que sean el origen para el siguiente
+      map.addMarker({lat: posicionGEO[i][0], lng: posicionGEO[i][1]});
+      lat = posicionGEO[i][0];
+      lng = posicionGEO[i][1];
+    };
   };
 
   function geolocalizar(){
@@ -26,7 +90,7 @@ $(function(){
       success: function(position){
         lat = position.coords.latitude;  // guarda coords en lat y lng
         lng = position.coords.longitude;
-
+        posicionGEO = [];
         map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
           el: '#map',
           lat: lat,
@@ -34,6 +98,8 @@ $(function(){
           click: enlazarMarcador,
           tap: enlazarMarcador
         });
+        posicionGEO.push([lat,lng]);
+        localStorage.posiciones = JSON.stringify(posicionGEO);
         map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
       },
       error: function(error) { alert('Ahí un error en la Geolocalización: '+error.message); },
@@ -41,5 +107,8 @@ $(function(){
     });
   };
 
-  geolocalizar();
+  $("#reinicia").on('click', reiniciar);
+  $("#compactar").on('click', compactar);
+
+  if (localStorage.posiciones.length != 0) { registra();} else {geolocalizar();};
 });
